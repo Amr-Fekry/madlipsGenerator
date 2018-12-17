88270806bf62cb4d23422fda89b7ef5b42e744cd
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-17 18:17:29 +0200

    new problem: words to be replaced are user input

--- madlipsGenerator.py
+++ madlipsGenerator.py
@@ -1,32 +1,15 @@
-# replacing the words [NOUN, VERB] in a string using replace() method
+# initial inputs: a list containing parts of speech (POSs) to be replaced, and a sentence
+# intermediate inputs: replacements  for POSs in the sentence one-by-one
 
-from random import randint
+# final output: a sentence with POSs replaced with user inputs
 
-# randomly generating replacements for NOUN and VERB
-def random_verb():
-	"""returns one of two verbs randomly"""
-	random_num = randint(0, 1)
-	if random_num == 0:
-	    return "run"
-	else:
-	    return "kayak"
+# hard-codes inputs for testing 
+parts_of_speech  = ["PLACE", "PERSON", "PLURALNOUN", "NOUN"]
+sentence = """This is PLACE, no NOUN named PERSON, We have so many PLURALNOUN around here."""
 
-def random_noun():
-	"""returns one of two nouns randomly"""
-	random_num = randint(0,1)
-	if random_num == 0:
-	    return "sofa"
-	else:
-	    return "llama"
-
-# using replace() method:
-
-def process_madlib(mad_lib):
-	"""takes a string and replaces [NOUN,VERB] with 1 of 2 nouns/verbs"""
-	return mad_lib.replace("NOUN",random_noun()).replace("VERB",random_verb())
-
-
-test_string_1 = "This is a good NOUN to use when you VERB your food"
-test_string_2 = "I'm going to VERB to the store and pick up a NOUN or two."
-print process_madlib(test_string_1)
-print process_madlib(test_string_2)
+# pseudo code:
+# split sentence into a list of words
+# for each word, check if it contains a part of speech 
+# if it does, prompt user for replacement then add it to list2
+# if not add the word to list2
+# convert list2 into a string
