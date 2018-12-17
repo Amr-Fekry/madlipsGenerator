d90eb0b63a89954e20f53d948f3bb61b21342708
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-17 18:36:57 +0200

    add pos_in_word() to check if a word contains a PartOfSpeech

--- madlipsGenerator.py
+++ madlipsGenerator.py
@@ -13,3 +13,19 @@ sentence = """This is PLACE, no NOUN named PERSON, We have so many PLURALNOUN ar
 # if it does, prompt user for replacement then add it to list2
 # if not add the word to list2
 # convert list2 into a string
+
+def pos_in_word(parts_of_speech, word):
+	"""returns the part of speech if it is a substring in the word. otherwise, None"""
+	for pos in parts_of_speech:
+		if pos in word:
+			return pos
+	return None
+
+
+test_cases = ["NOUN", "FALSE", "<<@PERSON><", "PLURALNOUN"]
+parts_of_speech2 = ["PERSON", "PLURALNOUN", "NOUN"]
+
+print pos_in_word(parts_of_speech2, test_cases[0])
+print pos_in_word(parts_of_speech2, test_cases[1])
+print pos_in_word(parts_of_speech2, test_cases[2])
+print pos_in_word(parts_of_speech2, test_cases[3])
