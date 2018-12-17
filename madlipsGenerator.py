714ef323e598e2043054179a3f529ec14d54cc2b
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-17 15:39:15 +0200

    randomly generating 1of2 replacements for NOUN and VERB

--- madlipsGenerator.py
+++ madlipsGenerator.py
@@ -1,4 +1,5 @@
 # replacing the words [NOUN, VERB] in a string using string slicing and concatenation
+from random import randint
 
 sentence = "A NOUN went on a walk. It can VERB really fast."
 
@@ -10,19 +11,27 @@ substring1 = sentence[:noun_position]
 substring2 = sentence[noun_position+4 : verb_position]
 substring3 = sentence[verb_position+4 :]
 
-# replacements for NOUN and VERB
-noun_replacement = "book" 
-verb_replacement = "read" 
+# randomly generating replacements for NOUN and VERB
+def random_verb():
+    random_num = randint(0, 1)
+    if random_num == 0:
+        return "run"
+    else:
+        return "kayak"
+        
+def random_noun():
+    random_num = randint(0,1)
+    if random_num == 0:
+        return "sofa"
+    else:
+        return "llama"
 
 # concatenate substrings with replacements
 new_sentence = ""
 new_sentence += substring1
-new_sentence += noun_replacement
+new_sentence += random_noun()
 new_sentence += substring2
-new_sentence += verb_replacement
+new_sentence += random_verb()
 new_sentence += substring3
 
 print new_sentence
-
-#or
-#new_sentence = substring1 + noun_replacement + substring2 + verb_replacement + substring3
